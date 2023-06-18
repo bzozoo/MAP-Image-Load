1ea3331f47368bc456254f03da4c9532571543fe
Bata Zoltán <batazo.dad@gmail.com>
2023-06-18 23:44:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { NProduct } from './data';
+import React from "react";
+import { NProduct } from "./data";
 
 function App() {
-  console.log(NProduct)
   return (
-  <div>
-      {/* <img src={require('./assets/img/unsplash1.jpg')} alt="" /> */} 
+    <div>
+      {/* <img src={require('./assets/img/unsplash1.jpg')} alt="" /> */}
       {NProduct.map((product) => (
-      <img key={product.id} src={product.thumbnails[0].img1}default alt={product.name}/>
-        ))}
+        <ImageComponent
+          key={product.id}
+          src={product.thumbnails[0].img1}
+          productName={product.name}
+        />
+      ))}
     </div>
   );
+}
+
+function ImageComponent({ src, productName }) {
+  const [image, setImage] = React.useState(null);
+
+  React.useEffect(() => {
+    import(src).then((image) => setImage(image));
+  }, [src]);
+
+  React.useEffect(() => {
+    console.log(JSON.stringify(image));
+  }, [image]);
+
+  return <img src={image?.default} alt={productName} />;
 }
 
 export default App;
